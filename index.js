3710ba75a017d3e4498a9efdd14cb71398a553ca
czx <czx95110@163.com>
2017-08-28 11:09:11 +0800

    to home done

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
-var panel = $('#gamePanel');
-var html = '';
-var row = 19;//行
-var col = 19;//列
-var cellList = [];
-for (var i = 0; i < row; i++) {
-  for (var j = 0; j < col; j++) {
 
-      if(i==0) {
-        if (j == 0) {
-          html += addCell('top left', i, j);
+(function(){
+  var panel = $('#gamePanel');
+  var html = '';
+  var row = 19;//行
+  var cellList = [];
+  var gameFive = {
+    init: function () {
+      for (var i = 0; i < row; i++) {
+        for (var j = 0; j < row; j++) {
+          if(i==0) {
+            if (j == 0) {
+              html += addCell('top left', i, j);
 
-        } else if (j == col - 1) {
-          html += addCell('top right', i, j);
-        } else {
-          html += addCell('top', i, j);
+            } else if (j == row - 1) {
+              html += addCell('top right', i, j);
+            } else {
+              html += addCell('top', i, j);
+            }
+          }else if(i==row-1){
+            if (j == 0) {
+              html += addCell('bottom left', i, j);
+
+            } else if (j == row - 1) {
+              html += addCell('bottom right', i, j);
+            } else {
+              html += addCell('bottom', i, j);
+            }
+          } else  if (j == 0) {
+            html += addCell(' left', i, j);
+
+          } else if (j == row - 1) {
+            html += addCell(' right', i, j);
+          } else {
+            html += addCell('', i, j);
+          }
         }
-      }else if(i==row-1){
-        if (j == 0) {
-          html += addCell('bottom left', i, j);
-
-        } else if (j == col - 1) {
-          html += addCell('bottom right', i, j);
-        } else {
-          html += addCell('bottom', i, j);
-        }
-      } else  if (j == 0) {
-          html += addCell(' left', i, j);
-
-        } else if (j == col - 1) {
-          html += addCell(' right', i, j);
-        } else {
-          html += addCell('', i, j);
-        }
+      }
+      panel.html(html);
+    }
+  };
 
 
-
+  function addCell(addCell, i, j) {
+    //return '<b id="cid_'+i+'_'+j+'" class="cell '+addCell+'"></b>';
+    return `<b id="cid_${i}_${j}" class="cell ${addCell}"></b>`;
   }
-}
-function addCell(addCell, i, j) {
-  //return '<b id="cid_'+i+'_'+j+'" class="cell '+addCell+'"></b>';
-  return `<b id="cid_${i}_${j}" class="cell ${addCell}"></b>`;
-}
+  var cellWidth=parseFloat($('.cell').css('width'));
 
-var cellWidth=parseFloat($('.cell').css('width'));
-console.log(cellWidth);
-panel.html(html);
-panel.css({
-  width:cellWidth*row,
-  height:cellWidth*col
-});
+  panel.css({
+    width:cellWidth*row,
+    height:cellWidth*row
+  });
 
 
+  gameFive.init();
 
-
+})();
 
 
 
